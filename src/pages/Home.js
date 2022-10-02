86814a5b83628be3d3d04481c0e5d1368861197b
adilett02 <adiletturdakunov2002@gmail.com>
2022-10-02 17:49:49 +0600

    separation into components

---
+++ src/pages/Home.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Categories from '../components/Categories';
+import PizzaBlock from '../components/PizzaBlock';
+import Sort from '../components/Sort';
+
+function Home() {
+  return (
+    <div className="content">
+      <div className="container">
+        <div className="content__top">
+          <Categories />
+          <Sort />
+        </div>
+        <h2 className="content__title">Все пиццы</h2>
+        <div className="content__items">
+          <PizzaBlock />
+          <PizzaBlock />
+          <PizzaBlock />
+          <PizzaBlock />
+          <PizzaBlock />
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Home;
